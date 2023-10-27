448da7d17feb18a51d4c8ae0f399b30eb292c162
nao008 <naoya-0829@i.softbank.jp>
2023-10-27 20:12:40 +0900

    Ask for camera permission on login and sign-up screens

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',
+                 BASE_DIR / 'templates' / 'allauth'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
